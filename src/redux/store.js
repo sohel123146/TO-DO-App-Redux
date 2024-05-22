d52ab53e0ddebb012ad5bb081c06711c0a31cbf5
sohel123146 <63089979+sohel123146@users.noreply.github.com>
2024-05-22 09:06:59 +0530

    Minor changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counter/counterSlice'
 import todoReducer from './todo/todoSlice'
-import taskState from './task/taskState'
+
 
 export default configureStore({
   reducer: {
-    counter:counterReducer,
     todo:todoReducer,
-    task:taskState,
   },
 })
